a0d80f2ec573d41ed49705180f8d99195a0fa127
ChrisBenua <kpbenua@edu.hse.ru>
2018-09-11 19:19:17 +0300

    Small update, rename

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter the Voltage: U, and Resistor's Resistance: R");
+        Console.WriteLine("Enter the Voltage: U, and Resistor's Resistance: R (In One Line)");
         int[] data = new int[2];
         try
         {
@@ -16,6 +16,9 @@ class Program
         }
         catch (System.FormatException) {
             Console.WriteLine("Something Wrong With Your Input");
+        }
+        catch (DivideByZeroException) {
+            Console.WriteLine("Don't Let Your Resistance Be Zero");
         }
         Console.WriteLine("Press Enter to Exit");
         Console.ReadLine();
